f5cacf72a3b98423e21ad2dbce21ffc5a03ce58c
Yu Qi Zhang <jerzhang@redhat.com>
2019-06-06 10:32:26 -0400

    Fix modules to a working state
    Fix module-setup and add a test-metal.service

--- dracut/30afterburn/module-setup.sh
+++ dracut/30afterburn/module-setup.sh
@@ -2,6 +2,10 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
+check() {
+    return 0
+}
+
 depends() {
     echo systemd network
 }
@@ -11,4 +15,13 @@ install() {
 
     inst_simple "$moddir/azure-hostname.service" \
         "$systemdutildir/system/azure-hostname.service"
+
+    mkdir -p "$initdir/$systemdsystemunitdir/initrd.target.requires"
+
+    ln -s "../azure-hostname.service" "$initdir/$systemdsystemunitdir/initrd.target.requires/azure-hostname.service"
+
+    inst_simple "$moddir/test-metal.service" \
+        "$systemdutildir/system/test-metal.service"
+
+    ln -s "../test-metal.service" "$initdir/$systemdsystemunitdir/initrd.target.requires/test-metal.service"
 }
